54ef1f8bbdab91c97873a50a85a803771cab3c73
Denis Koljada <10576274+justdeko@users.noreply.github.com>
2025-12-07 14:26:42 +0100

    use other maven publish plugin

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,8 +6,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    id("maven-publish")
-    id("signing")
+    alias(libs.plugins.mavenPublish)
 }
 
 group = "io.github.justdeko"
@@ -19,9 +18,6 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
         publishLibraryVariants("release")
-        mavenPublication {
-            artifactId = "kuiver-android"
-        }
     }
 
     listOf(
@@ -68,76 +64,44 @@ android {
     }
 }
 
-publishing {
-    publications {
-        withType<MavenPublication> {
-            pom {
-                name.set("Kuiver")
-                description.set("A Compose Multiplatform library for visualizing and interacting with directed graphs")
-                url.set("https://github.com/justdeko/kuiver")
+val mavenCentralUsername = System.getenv("CENTRAL_PORTAL_USERNAME") ?: ""
+val mavenCentralPassword = System.getenv("CENTRAL_PORTAL_PASSWORD") ?: ""
+val signingKey = System.getenv("SIGNING_KEY") ?: ""
+val signingPassword = System.getenv("SIGNING_PASSWORD") ?: ""
+ext.set("mavenCentralUsername", mavenCentralUsername)
+ext.set("mavenCentralPassword", mavenCentralPassword)
+ext.set("signingInMemoryKey", signingKey)
+ext.set("signingInMemoryKeyPassword", signingPassword)
 
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
 
-                developers {
-                    developer {
-                        id.set("dk")
-                        name.set("Denis Koljada")
-                    }
-                }
+    coordinates("io.github.justdeko", "kuiver", version.toString())
 
-                scm {
-                    connection.set("scm:git:git://github.com/justdeko/kuiver.git")
-                    developerConnection.set("scm:git:ssh://github.com/justdeko/kuiver.git")
-                    url.set("https://github.com/justdeko/kuiver")
-                }
+    pom {
+        name.set("Kuiver")
+        description.set("A Compose Multiplatform library for visualizing and interacting with directed graphs")
+        url.set("https://github.com/justdeko/kuiver")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
         }
-    }
 
-    repositories {
-        mavenLocal()
-
-        maven {
-            name = "centralPortal"
-            val isSnapshot = version.toString().endsWith("-SNAPSHOT")
-            url = if (isSnapshot) {
-                // Snapshots go to the snapshots repository
-                uri("https://central.sonatype.com/repository/maven-snapshots/")
-            } else {
-                // Releases go to the publisher upload API
-                uri("https://central.sonatype.com/api/v1/publisher/upload?publishingType=USER_MANAGED")
-            }
-            credentials {
-                username = System.getenv("CENTRAL_PORTAL_USERNAME") ?: project.findProperty("centralPortalUsername") as String? ?: ""
-                password = System.getenv("CENTRAL_PORTAL_PASSWORD") ?: project.findProperty("centralPortalPassword") as String? ?: ""
+        developers {
+            developer {
+                id.set("dk")
+                name.set("Denis Koljada")
             }
         }
-    }
-}
 
-signing {
-    // Use in-memory ASCII-armored keys from environment variables
-    val signingKey = System.getenv("SIGNING_KEY")
-    val signingPassword = System.getenv("SIGNING_PASSWORD")
-
-    if (signingKey != null && signingPassword != null) {
-        useInMemoryPgpKeys(signingKey, signingPassword)
-        sign(publishing.publications)
-    }
-}
-
-// Configure artifact IDs after publications are created
-afterEvaluate {
-    publishing.publications.withType<MavenPublication> {
-        artifactId = when (name) {
-            "kotlinMultiplatform" -> "kuiver"
-            "androidRelease" -> "kuiver-android"
-            else -> "kuiver-${name.lowercase()}"
+        scm {
+            connection.set("scm:git:git://github.com/justdeko/kuiver.git")
+            developerConnection.set("scm:git:ssh://github.com/justdeko/kuiver.git")
+            url.set("https://github.com/justdeko/kuiver")
         }
     }
 }
